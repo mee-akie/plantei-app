053dd3604af4c092de082c0c2ab0e17dfd83a67d
Melissa <melissainui@gmail.com>
2022-08-29 11:22:27 -0300

    [WIP] add spring

---
+++ src/main/java/com/plantei/planteibackend/PlanteiBackendApplication.java
@@ -0,0 +1,15 @@
+package com.plantei.planteibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanteiBackendApplication {
+
+    public static void main(String[] args) {
+
+        //temporario
+        SpringApplication.run(PlanteiBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/plantei/planteibackend/securingweb/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.plantei.planteibackend.securingweb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    /**
+     * Esse Bean define quais URLs devem ter autenticacao ou nao.
+     * <p>
+     * URLs que precisam ser autenticadas: "/", "/home".
+     * <p>
+     * URLs que NAO precisam ser autenticadas: as demais.
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .antMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
