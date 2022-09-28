4e13f08ee55f3e22e10c36f6388b52550948d115
RafaelNaj <91430231+najimpx@users.noreply.github.com>
2022-09-28 09:36:19 -0300

    Delete RepositorioReceita.java

--- src/main/java/com/plantei/planteibackend/repository/RepositorioReceita.java
+++
@@ -1,9 +0,0 @@
-package com.plantei.planteibackend.repository;
-
-import com.plantei.planteibackend.model.Receita;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositorioReceita extends JpaRepository<Receita, Long> {
-}
