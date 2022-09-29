274944266dc388411b2eff9efb956986e3629ca5
Melissa Inui <melissainui@gmail.com>
2022-09-29 19:40:26 -0300

    WIP - fix FK

--- src/main/java/com/plantei/planteibackend/model/Planta.java
+++ src/main/java/com/plantei/planteibackend/model/Planta.java
@@ -1,15 +1,24 @@
 package com.plantei.planteibackend.model;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import javax.persistence.*;
 
 @Entity
+@Table(name = "planta")
 public class Planta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID_planta")
+    @Column
     private Long id;
     @Column(nullable = false)
     private String nome;
+
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id", referencedColumnName = "id_planta")
+    private DescricaoPlanta descricaoPlanta;
 
     public Planta() {
     }
@@ -22,7 +31,15 @@ public class Planta {
         return nome;
     }
 
+    public DescricaoPlanta getDescricaoPlanta() {
+        return descricaoPlanta;
+    }
+
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public void setDescricaoPlanta(DescricaoPlanta descricaoPlanta) {
+        this.descricaoPlanta = descricaoPlanta;
     }
 }
