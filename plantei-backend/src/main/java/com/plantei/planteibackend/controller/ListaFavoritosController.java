aa7d4f9430939cd70d2df8ed73055635ce09945d
RafaelNaj <91430231+najimpx@users.noreply.github.com>
2022-11-23 01:36:02 -0300

    Controle e Repositorio Lista Favoritos get id_planta com id_usuario

--- plantei-backend/src/main/java/com/plantei/planteibackend/controller/ListaFavoritosController.java
+++ plantei-backend/src/main/java/com/plantei/planteibackend/controller/ListaFavoritosController.java
@@ -3,6 +3,7 @@ package com.plantei.planteibackend.controller;
 import com.plantei.planteibackend.exception.ResourceNotFoundException;
 import com.plantei.planteibackend.model.ListaFavoritos;
 import com.plantei.planteibackend.model.ListaFavoritosId;
+import com.plantei.planteibackend.model.Planta;
 import com.plantei.planteibackend.repository.RepositorioListaFavoritos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -29,6 +30,12 @@ public class ListaFavoritosController {
         return ResponseEntity.ok().body(dadosPlanta);
     }
 
+    @GetMapping("/listaUsuario/{id_usuario}")
+    public List<Long> getListaByUserAndPlant(@PathVariable long id_usuario) throws ResourceNotFoundException {
+        List<Long> dadosLista = repositorioListaFavoritos.findListaByUserAndPlant(id_usuario);
+        return dadosLista;
+    }
+    
     @PostMapping(path = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ListaFavoritos> addFavoriteList(@RequestBody ListaFavoritos listaFavoritos) {
         repositorioListaFavoritos.save(listaFavoritos);
